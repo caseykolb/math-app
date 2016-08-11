728b81ea611ac8337edf419853a606420e764eb5
caseykolb <caseypkolb@gmail.com>
2016-08-11 17:47:27 -0400

    Add functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Card from './Card'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      correct: 0,
+      incorrect: 0,
+      time: 15,
+      question: null,
+      answer: null
+    }
+  }
+
+  componentDidMount() {
+    this.newQuestion();
+  }
+
+  checkAnswer(e) {
+    // enter pressed
+    if (e.which === 13) {
+      let answer = parseInt(e.target.value, 10)
+      e.target.value = null;
+      if (answer === this.state.answer)
+        this.handleSuccess()
+      else
+        this.handleFailure()
+    } 
+  }
+
+  handleSuccess() {
+    this.setState({ correct: this.state.correct + 1 })
+    this.newQuestion();
+  }
+
+  handleFailure() {
+    this.setState({ incorrect: this.state.incorrect + 1 })
+    this.newQuestion();
+  }
+
+  newQuestion() {
+    // random numbers between 1 and 12
+    let x = Math.floor(Math.random() * 12) + 1;
+    let y = Math.floor(Math.random() * 12) + 1;
+
+    let coinToss = (Math.floor(Math.random()) + 1) === 0 ? true : false;
+    let answer = coinToss ? (x / y) : (x * y);
+    let operator = coinToss ? '/' : 'x';
+
+    this.setState({
+      question: x.toString() + ' ' + operator + ' ' + y.toString() + ' =',
+      answer: answer 
+    })
+    return 
+  }
+  
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <h1 className='Title'>Math Flash Cards</h1>
+        <Card
+          question={this.state.question}
+          time={this.state.time}
+          correct={this.state.correct}
+          incorrect={this.state.incorrect}
+          onFailure={this.handleFailure.bind(this)}
+          onSubmit={this.checkAnswer.bind(this)}
+        />
       </div>
     );
   }
--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Paper, TextField, Badge } from 'material-ui';
+import { Grid, Row, Col } from 'react-bootstrap'
+import CheckIcon from 'material-ui/svg-icons/action/done';
+import XIcon from 'material-ui/svg-icons/content/add';
+import ReactCountdownClock from 'react-countdown-clock';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import './Card.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+var Card = (props) => {
+  return  <div>
+            <MuiThemeProvider>
+              <Paper className='Card' zDepth={2}>
+                <Grid fluid>
+                  <Row className='Card-Contents'>
+                      <Col xs={4} sm={4} md={4} lg={4} className='Countdown'>
+                        <div id='clock'>
+                          <ReactCountdownClock 
+                            seconds={props.time}
+                            showMilliseconds={true}
+                            color="purple"
+                            alpha={0.9}
+                            size={100}
+                            onComplete={props.onFailure} 
+                          />
+                        </div>
+                      </Col>
+                      <Col xs={4} sm={4} md={4} lg={4} className='Question'>
+                        <h1>{props.question}</h1>
+                        <TextField
+                          name='Answer' 
+                          hintText='Answer'
+                          style={{width: 60}}
+                          onKeyDown={props.onSubmit.bind(this)}
+                        />
+                      </Col>
+                      <Col xs={4} sm={4} md={4} lg={4} className='Badges'>
+                        <Badge 
+                          badgeContent={props.correct} 
+                          secondary={true} 
+                          badgeStyle={{top: 12, right: 12}}
+                        >
+                          <CheckIcon />
+                        </Badge>
+                        <Badge 
+                          badgeContent={props.incorrect} 
+                          primary={true} 
+                          badgeStyle={{top: 12, right: 12}}
+                        >
+                          <XIcon className='xIcon' />
+                        </Badge>
+                      </Col>
+                  </Row>
+                </Grid>
+              </Paper>
+            </MuiThemeProvider>
+          </div>
 }
 
-export default App;
+export default Card;
